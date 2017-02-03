63ddbdbfecd7d9533da04f8799f42c8ea915afd8
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 22:50:05 -0500

    added route /status

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,14 @@
-var linkGen = require('../shortUrl');
+var linkGen = require('../shortURL');
 
 module.exports = (express) => {
   var router = express.Router();
 
   router.get('/', (req, res) => {
     res.json({ Hello: 'World' });
+  });
+
+  router.get('/status', (req, res) => {
+    res.json({ Status: 'True' });
   });
 
   return router;
