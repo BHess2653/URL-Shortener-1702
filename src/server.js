63dc480bef68441cc679f5dd9b09bbe75f2caee4
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 21:09:19 -0500

    updated server.js file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
 
-var port = 3000;
+var port = 3000; // port set to 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("Server active on ", port);
+});
