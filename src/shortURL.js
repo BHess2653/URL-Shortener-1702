8c1b6c62d8d5856e934b9f2fe83bc24ec3002d5d
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 22:24:48 -0500

    updated shortURL.js added return

--- src/shortURL.js
+++ src/shortURL.js
@@ -8,4 +8,11 @@ for(var i = 0, i < 5, i++){
 short =+ randomText.charAt(Math.floor(Math.random() * randomText.length));
 }
 
+var myURL = 'http://sho.rt/';
+// http://bit.ly/1bdDlXc
+
+return {
+  origUrl: urlLink, // This is the link the user enters
+  shortUrl: myURL + short // This will be the Shortened version
+};
 };
