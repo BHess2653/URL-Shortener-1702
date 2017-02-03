69ee42e2a6f6dd2e2edca1e71cfee1eed5b3a130
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 22:47:00 -0500

    fixed issue in shortURL.js

--- src/shortURL.js
+++ src/shortURL.js
@@ -4,12 +4,11 @@ var short = '';
 var randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; // text to loop through
 
 // for loop
-for(var i = 0, i < 5, i++){
+for(var i = 0; i < 5; i++){
 short =+ randomText.charAt(Math.floor(Math.random() * randomText.length));
 }
 
 var myURL = 'http://sho.rt/';
-// http://bit.ly/1bdDlXc
 
 return {
   origUrl: urlLink, // This is the link the user enters
