8294d46cedbc569e509408378bf9b2abb7004d0a
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 22:42:59 -0500

    updated api.js added route

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,5 +3,9 @@ var linkGen = require('../shortUrl');
 module.exports = (express) => {
   var router = express.Router();
 
-  
+  router.get('/', (req, res) => {
+    res.json({ Hello: 'World' });
+  });
+
+  return router;
 };
