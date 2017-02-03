bec0088710c9ae2b1d6e50d266cff423edac56be
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 22:38:52 -0500

    updated server.js added /api/v1

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ var port = 3000; // port set to 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/v1', require('./routes/api')(express));
 
 app.listen(port, () => {
   console.log("Server active on ", port);
