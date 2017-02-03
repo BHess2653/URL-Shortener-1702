592546e3a76d59452278c83eff710dab81df00a3
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 21:50:00 -0500

    updated shortURL.js file

--- src/shortURL.js
+++ src/shortURL.js
@@ -1,6 +1,7 @@
-exports.short = () => {
+exports.short = (url) => {
 
-var randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var short = '';
+var randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; // text to loop through
 
 // for loop
 for(var i = 0, i < 5, i++){
