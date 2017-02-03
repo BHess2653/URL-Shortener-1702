c90097c6827daf27da84a03b7160c340be9e26ff
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 22:04:59 -0500

    updated shortURL.js for loop

--- src/shortURL.js
+++ src/shortURL.js
@@ -1,11 +1,11 @@
-exports.short = (url) => {
+exports.short = (urlLink) => {
 
 var short = '';
 var randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; // text to loop through
 
 // for loop
 for(var i = 0, i < 5, i++){
-
+short =+ randomText.charAt(Math.floor(Math.random() * randomText.length));
 }
 
 };
