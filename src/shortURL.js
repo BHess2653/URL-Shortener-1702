946f1a852a227fe5bb2a77b56aac12032c27206f
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 23:33:26 -0500

    updated files

--- src/shortURL.js
+++ src/shortURL.js
@@ -1,17 +1,17 @@
 exports.short = (urlLink) => {
 
-var short = '';
-var randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // text to loop through
+  var short = '';
+  var randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // text to loop through
 
-// for loop
-for(var i = 0; i < 8; i++){
-short += randomText.charAt(Math.floor(Math.random() * randomText.length));
-}
+  // for loop
+  for(var i = 0; i < 8; i++){
+    short += randomText.charAt(Math.floor(Math.random() * randomText.length));
+  }
 
-var myURL = 'http://sho.rt/';
+  var myURL = 'http://sho.rt/';
 
-return {
-  origUrl: urlLink, // This is the link the user enters
-  shortUrl: myURL + short // This will be the Shortened version
-};
+  return {
+    origUrl: urlLink, // This is the link the user enters
+    shortUrl: myURL + short // This will be the Shortened version
+  };
 };
