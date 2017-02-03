08c2cd089b283a866f210c8e557396ff1c6effbd
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-02 20:43:30 -0500

    created server.js file

---
+++ src/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
