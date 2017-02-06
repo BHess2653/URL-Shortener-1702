bfba845d10b07d141b532e68baa2b85e3e903094
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-06 18:58:17 -0500

    created routes/api/urls.js

---
+++ src/routes/api/urls.js
@@ -0,0 +1,10 @@
+const url = require('../../models/urls');
+const gen = require('../../models/urlGen');
+
+module.exports = (express) => {
+const router = express.Router();
+
+
+
+return router;
+};
