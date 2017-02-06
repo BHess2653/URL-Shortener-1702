0b7ea5db45f27c74e2b3ed2f411f273627a9c08c
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-06 18:48:44 -0500

    updated models urls.js add find one and find all

--- src/models/urls.js
+++ src/models/urls.js
@@ -3,3 +3,19 @@ const db = require('./db');
 exports.add = (data, err, success) => {
   db.url.create(data).then(success).catch(err);
 };
+
+exports.all = (err, success) => {
+  db.url.findAll().then(success).catch(err);
+};
+
+exports.one = (data, err, success) => {
+  db.url.find({
+    where: {
+      id: data.id,
+    },
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
+};
