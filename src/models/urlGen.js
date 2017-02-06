866f0119f28e735a44e1576c58ee9cbc58a539ac
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-06 16:55:46 -0500

    moved shortUrl to models folder and changed name to urlGen

---
+++ src/models/urlGen.js
@@ -0,0 +1,12 @@
+exports.shortUrl = () => {
+
+  let shortUrl = '';
+  const randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // text to loop through
+
+  // for loop
+  for(let i = 0; i < 8; i++){
+    shortUrl += randomText.charAt(Math.floor(Math.random() * randomText.length));
+  }
+
+  return shortUrl
+};
