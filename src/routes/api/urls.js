2eb31b38edab3fc54e38443cf8757600b0c889bf
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-06 19:04:59 -0500

    created routes urls.js added create

--- src/routes/api/urls.js
+++ src/routes/api/urls.js
@@ -4,7 +4,17 @@ const gen = require('../../models/urlGen');
 module.exports = (express) => {
 const router = express.Router();
 
-
+router.post('/urls', (req,res) => {
+  const reqBody = req.body;
+  reqBody.shortUrl = gen.shortUrl(url);
+  url.add(req.body,
+  (err) => {
+    res.status(500).json(err);
+  },
+  (data) => {
+    res.status(200).json(data)
+  });
+});
 
 return router;
 };
