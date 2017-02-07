5a4c6e77c1a0607b6e9a19f380e677ddd21dbf06
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-06 23:43:11 -0500

    updated api.js and changed name to index.js

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+module.exports = (express) => {
+  var router = express.Router();
+
+  router.get('/', (req, res) => {
+    res.json({ Hello: 'World' });
+  });
+
+  router.get('/status', (req, res) => {
+    res.json({ Status: 'True' });
+  });
+
+  router.use('/api/v1', require('./api/urls')(express));
+  router.use('/go/', require('./go/redirect')(express));
+
+  return router;
+};
