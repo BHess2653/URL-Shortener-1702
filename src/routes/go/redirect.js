c19571da303c419b08ba7d3cd6bd3a42d07eeab6
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-06 23:35:41 -0500

    updated redirect.js in routes

--- src/routes/go/redirect.js
+++ src/routes/go/redirect.js
@@ -3,7 +3,16 @@ const urls = require('../../models/urls');
 module.exports = (express) => {
   const router = express.Router();
 
-
+  router.get('/:shortUrl', (req, res) => {
+    req.body.shortUrl = req.params.shortUrl;
+    urls.go(req.body,
+    (err) => {
+      res.status(500).json(err);
+    },
+  (data) => {
+    res.redirect(data.origUrl);
+  });
+});
 
   return router;
 };
