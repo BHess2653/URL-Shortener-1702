6d382e131ab758ce9ecc46352cd460c40e735c69
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-09 22:13:30 -0500

    updated urlGen.js

--- src/models/urlGen.js
+++ src/models/urlGen.js
@@ -1,12 +1,12 @@
 exports.shortUrl = () => {
 
-  let shortUrl = '';
-  const randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // text to loop through
+    let shortUrl = '';
+    const randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // text to loop through
 
-  // for loop
-  for(let i = 0; i < 8; i++){
-    shortUrl += randomText.charAt(Math.floor(Math.random() * randomText.length));
-  }
+    // for loop
+    for (let i = 0; i < 8; i++) { // loops to length of 8
+        shortUrl += randomText.charAt(Math.floor(Math.random() * randomText.length));
+    }
 
-  return shortUrl
+    return shortUrl
 };
