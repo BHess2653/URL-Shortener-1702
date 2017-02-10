7140933cf385e2338b816efbc87a2d7a2c78bdd0
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-09 22:04:54 -0500

    updated urls.js in models with comments

--- src/models/urls.js
+++ src/models/urls.js
@@ -1,47 +1,65 @@
 const db = require('./db');
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Add Function Export
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.add = (data, err, success) => {
-  db.url.create(data).then(success).catch(err);
+    db.url.create(data).then(success).catch(err);
 };
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// All Function Export
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.all = (err, success) => {
-  db.url.findAll().then(success).catch(err);
+    db.url.findAll().then(success).catch(err);
 };
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// One Function Export
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.one = (data, err, success) => {
-  db.url.find({
-    where: {
-      id: data.id,
-    },
-    include: [{
-      all: true,
-      nested: true,
-    }],
-  }).then(success).catch(err);
+    db.url.find({
+        where: {
+            id: data.id,
+        },
+        include: [{
+            all: true,
+            nested: true,
+        }],
+    }).then(success).catch(err);
 };
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Update Function Export
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.update = (data, err, success) => {
-  db.url.find({
-    where: {
-      id: data.id,
-    },
-  }).then((existingData) => {
-    existingData.updateAttributes(data).then(success).catch(err);
-  }).catch(err);
+    db.url.find({
+        where: {
+            id: data.id,
+        },
+    }).then((existingData) => {
+        existingData.updateAttributes(data).then(success).catch(err);
+    }).catch(err);
 };
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Remove Function Export
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.remove = (data, err, success) => {
-  db.url.destroy({
-    where: {
-      shortUrl: data.shortUrl,
-    },
-  }).then(success).catch(err);
+    db.url.destroy({
+        where: {
+            shortUrl: data.shortUrl,
+        },
+    }).then(success).catch(err);
 };
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Redirect Function Export
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.go = (data, err, success) => {
-  db.url.find({
-    where: {
-      shortUrl: data.shortUrl,
-    },
-  }).then(success).catch(err);
+    db.url.find({
+        where: {
+            shortUrl: data.shortUrl,
+        },
+    }).then(success).catch(err);
 };
