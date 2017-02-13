9e40e73e2ae45e55dfa888e8857d027da3245c87
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-12 21:57:00 -0500

    updated for eslint

--- src/models/urls.js
+++ src/models/urls.js
@@ -5,68 +5,68 @@ const db = require('./db');
 // Add Function Export
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.add = (data, err, success) => {
-    db.url.create(data).then(success).catch(err);
-    util.debug("Url was Added from Models ", data);
+  db.url.create(data).then(success).catch(err);
+  util.debug('Url was Added from Models ', data);
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // All Function Export
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.all = (err, success) => {
-    db.url.findAll().then(success).catch(err);
-    util.debug("Urls found from Models");
+  db.url.findAll().then(success).catch(err);
+  util.debug('Urls found from Models ');
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // One Function Export
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.one = (data, err, success) => {
-    db.url.find({
-        where: {
-            id: data.id,
-        },
-        include: [{
-            all: true,
-            nested: true,
-        }],
-    }).then(success).catch(err);
-    util.debug("Url found in Models ", data);
+  db.url.find({
+    where: {
+      id: data.id,
+    },
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
+  util.debug('Url found in Models ', data);
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Update Function Export
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.update = (data, err, success) => {
-    db.url.find({
-        where: {
-            id: data.id,
-        },
-    }).then((existingData) => {
-        existingData.updateAttributes(data).then(success).catch(err);
-    }).catch(err);
-    util.debug("Url Updated from Models ", data);
+  db.url.find({
+    where: {
+      id: data.id,
+    },
+  }).then((existingData) => {
+    existingData.updateAttributes(data).then(success).catch(err);
+  }).catch(err);
+  util.debug('Url Updated from Models ', data);
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Remove Function Export
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.remove = (data, err, success) => {
-    db.url.destroy({
-        where: {
-            shortUrl: data.shortUrl,
-        },
-    }).then(success).catch(err);
-    util.debug("Url Removed from Models ", data);
+  db.url.destroy({
+    where: {
+      shortUrl: data.shortUrl,
+    },
+  }).then(success).catch(err);
+  util.debug('Url Removed from Models ', data);
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Redirect Function Export
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 exports.go = (data, err, success) => {
-    db.url.find({
-        where: {
-            shortUrl: data.shortUrl,
-        },
-    }).then(success).catch(err);
-    util.debug("shortUrl found in Models ", data);
+  db.url.find({
+    where: {
+      shortUrl: data.shortUrl,
+    },
+  }).then(success).catch(err);
+  util.debug('shortUrl found in Models ', data);
 };
