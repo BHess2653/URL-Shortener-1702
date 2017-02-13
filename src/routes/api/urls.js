e18fce2b3fccd994b090959bf76ef2fae4efcb41
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-12 22:01:01 -0500

    updated for eslint

--- src/routes/api/urls.js
+++ src/routes/api/urls.js
@@ -2,80 +2,80 @@ const url = require('../../models/urls');
 const gen = require('../../models/urlGen');
 
 module.exports = (express) => {
-    const router = express.Router();
+  const router = express.Router();
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Create Url
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    router.post('/urls', (req, res) => {
-        const reqBody = req.body;
-        reqBody.shortUrl = gen.shortUrl(url);
-        url.add(req.body,
-            (err) => { // errorCallback
-                res.status(500).json(err);
-            },
-            (data) => { // successCallback
-                res.status(200).json(data)
-            });
-    });
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // Create Url
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  router.post('/urls', (req, res) => {
+    const reqBody = req.body;
+    reqBody.shortUrl = gen.shortUrl(url);
+    url.add(req.body,
+        (err) => { // errorCallback
+          res.status(500).json(err);
+        },
+        (data) => { // successCallback
+          res.status(200).json(data);
+        });
+  });
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Find All Urls
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    router.get('/urls', (req, res) => {
-        url.all(
-            (err) => { // errorCallback
-                res.status(500).json(err);
-            },
-            (data) => { // successCallback
-                res.status(200).json(data);
-            });
-    });
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // Find All Urls
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  router.get('/urls', (req, res) => {
+    url.all(
+        (err) => { // errorCallback
+          res.status(500).json(err);
+        },
+        (data) => { // successCallback
+          res.status(200).json(data);
+        });
+  });
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Find One Url by ID
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    router.get('/urls/:id', (req, res) => {
-        const reqBody = req.body;
-        reqBody.id = req.params.id;
-        url.one(req.body,
-            (err) => { // errorCallback
-                res.status(500).json(err);
-            },
-            (data) => { // successCallback
-                res.status(200).json(data)
-            });
-    });
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // Find One Url by ID
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  router.get('/urls/:id', (req, res) => {
+    const reqBody = req.body;
+    reqBody.id = req.params.id;
+    url.one(req.body,
+        (err) => { // errorCallback
+          res.status(500).json(err);
+        },
+        (data) => { // successCallback
+          res.status(200).json(data);
+        });
+  });
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Update Url by ID
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    router.post('/urls/:id', (req, res) => {
-        const reqBody = req.body;
-        reqBody.id = req.params.id;
-        url.update(req.body,
-            (err) => { // errorCallback
-                res.status(500).json(err);
-            },
-            (data) => { // successCallback
-                res.status(200).json(data)
-            });
-    });
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // Update Url by ID
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  router.post('/urls/:id', (req, res) => {
+    const reqBody = req.body;
+    reqBody.id = req.params.id;
+    url.update(req.body,
+        (err) => { // errorCallback
+          res.status(500).json(err);
+        },
+        (data) => { // successCallback
+          res.status(200).json(data);
+        });
+  });
 
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    // Delete Url by ID
-    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    router.delete('/urls/:id', (req, res) => {
-        const reqBody = req.body;
-        reqBody.id = req.params.id;
-        url.remove(req.body,
-            (err) => { // errorCallback
-                res.status(500).json(err);
-            },
-            (data) => { // successCallback
-                res.status(200).json(data)
-            });
-    });
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // Delete Url by ID
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  router.delete('/urls/:id', (req, res) => {
+    const reqBody = req.body;
+    reqBody.id = req.params.id;
+    url.remove(req.body,
+        (err) => { // errorCallback
+          res.status(500).json(err);
+        },
+        (data) => { // successCallback
+          res.status(200).json(data);
+        });
+  });
 
-    return router;
+  return router;
 };
