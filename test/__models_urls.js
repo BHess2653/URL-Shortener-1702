006b3049451f6917b80bc10f686b267a63f98acc
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-12 23:33:33 -0500

    created __models_urls.js in test folder

---
+++ test/__models_urls.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const url = require('../src/models/urls');
+const gen = require('../src/models/urlGen');
+const util = require('../lib/util');
+
+describe('Urls Model', () => {
+
+
+  
+});
