0f30b83828be0ac55951353863269c81cfb0e6a1
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-12 22:06:47 -0500

    updated for eslint

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 const util = require('../lib/util');
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 const port = process.env.PORT || 3000; // port set to 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true,
 }));
 app.use('/', require('./routes')(express));
 
 const server = app.listen(port, () => {
-    util.debug("Server active on port ", port);
+  util.debug('Server active on port ', port);
 });
 
 module.exports = server;
