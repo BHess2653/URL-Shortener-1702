9749aea525e5dd6ba1c4c23a07ce6494ace1f6f2
BHess2653 <GM_Dahaka@BHMac.local>
2017-02-12 21:51:59 -0500

    updated for eslint

--- src/models/urlGen.js
+++ src/models/urlGen.js
@@ -1,12 +1,10 @@
 exports.shortUrl = () => {
+  let shortUrl = '';
+  const randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // text to loop through
 
-    let shortUrl = '';
-    const randomText = 'abcdefghijlkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; // text to loop through
-
-    // for loop
-    for (let i = 0; i < 8; i++) { // loops to length of 8
-        shortUrl += randomText.charAt(Math.floor(Math.random() * randomText.length));
-    }
-
-    return 'http://sho.rt/' + shortUrl
+  // for loop
+  for (let i = 0; i < 8; i++) { // loops to length of 8
+    shortUrl += randomText.charAt(Math.floor(Math.random() * randomText.length));
+  }
+  return 'http://sho.rt/' + shortUrl;
 };
